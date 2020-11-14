120f030cd92b517bbca1d2b870dec487a0141bc4
Matt Osterfield <60369843+OstyPowers@users.noreply.github.com>
2020-11-14 14:39:06 +1300

    Add files via upload

---
+++ View/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace View
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.LevelNameDisplay = new System.Windows.Forms.Label();
+            this.MoveCounter = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UpButton
+            // 
+            this.UpButton.Location = new System.Drawing.Point(265, 338);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(75, 23);
+            this.UpButton.TabIndex = 0;
+            this.UpButton.Text = "Up";
+            this.UpButton.UseVisualStyleBackColor = true;
+            this.UpButton.Click += new System.EventHandler(this.UpButton_Click);
+            // 
+            // RightButton
+            // 
+            this.RightButton.Location = new System.Drawing.Point(302, 367);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(75, 23);
+            this.RightButton.TabIndex = 1;
+            this.RightButton.Text = "Right";
+            this.RightButton.UseVisualStyleBackColor = true;
+            this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.Location = new System.Drawing.Point(221, 367);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(75, 23);
+            this.LeftButton.TabIndex = 2;
+            this.LeftButton.Text = "Left";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            this.LeftButton.Click += new System.EventHandler(this.LeftButton_Click);
+            // 
+            // DownButton
+            // 
+            this.DownButton.Location = new System.Drawing.Point(265, 396);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(75, 23);
+            this.DownButton.TabIndex = 3;
+            this.DownButton.Text = "Down";
+            this.DownButton.UseVisualStyleBackColor = true;
+            this.DownButton.Click += new System.EventHandler(this.DownButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(47, 367);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // LevelNameDisplay
+            // 
+            this.LevelNameDisplay.AutoSize = true;
+            this.LevelNameDisplay.Location = new System.Drawing.Point(564, 367);
+            this.LevelNameDisplay.Name = "LevelNameDisplay";
+            this.LevelNameDisplay.Size = new System.Drawing.Size(38, 15);
+            this.LevelNameDisplay.TabIndex = 5;
+            this.LevelNameDisplay.Text = "label1";
+            // 
+            // MoveCounter
+            // 
+            this.MoveCounter.AutoSize = true;
+            this.MoveCounter.Location = new System.Drawing.Point(525, 396);
+            this.MoveCounter.Name = "MoveCounter";
+            this.MoveCounter.Size = new System.Drawing.Size(38, 15);
+            this.MoveCounter.TabIndex = 6;
+            this.MoveCounter.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(443, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Move Count:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(443, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Current Level Name:";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(564, 323);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Select Level";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MoveCounter);
+            this.Controls.Add(this.LevelNameDisplay);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.DownButton);
+            this.Controls.Add(this.LeftButton);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.UpButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UpButton;
+        private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.Button DownButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label LevelNameDisplay;
+        private System.Windows.Forms.Label MoveCounter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ View/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TaM;
+
+namespace View
+{
+    public partial class Form1 : Form
+    {
+        private int _ticks;
+        Game game;
+        public Form1()
+        {
+            InitializeComponent();
+            this.game = new Game();
+            game.AddLevel("level 3", 7, 7,
+           "0303 0606 0001"
+           + " 1001 1000 1000 1000 1000 1000 1100"
+           + " 0001 0000 0000 0000 0000 0000 0100"
+           + " 0001 0000 0000 0000 0000 0000 0100"
+           + " 0001 0000 0000 0000 0000 0000 0100"
+           + " 0001 0000 0000 0000 0000 0000 0100"
+           + " 0001 0000 0000 0000 0000 0000 0100"
+           + " 0011 0010 0010 0010 0010 0010 0110");
+            game.AddLevel("level 2", 7, 7,
+            "0303 0300 0001"
+            + " 1001 1000 1000 1000 1000 1000 1100"
+            + " 0001 0000 0000 0000 0000 0000 0100"
+            + " 0001 0000 0000 0000 0000 0000 0100"
+            + " 0001 0000 0000 0000 0000 0000 0100"
+            + " 0001 0000 0000 0000 0000 0000 0100"
+            + " 0001 0000 0000 0000 0000 0000 0100"
+            + " 0011 0010 0010 0010 0010 0010 0110");
+            game.AddLevel("level 1", 3, 1, "0000 0001 0002 1011 1010 1110");
+            this.SetDisplayLevelName();
+            this.SetMoveCount();
+            this.SetLevel();
+
+
+
+        }
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+            this.game.MoveTheseus(Moves.UP);
+        }
+
+        private void RightButton_Click(object sender, EventArgs e)
+        {
+            this.game.MoveTheseus(Moves.RIGHT);
+        }
+
+        private void DownButton_Click(object sender, EventArgs e)
+        {
+            this.game.MoveTheseus(Moves.DOWN);
+        }
+
+        private void LeftButton_Click(object sender, EventArgs e)
+        {
+            this.game.MoveTheseus(Moves.LEFT);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void SetDisplayLevelName()
+        {
+            this.LevelNameDisplay.Text = this.game.CurrentLevelName;
+        }
+
+        private void SetMoveCount()
+        {
+            this.MoveCounter.Text = this.game.MoveCount.ToString();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            _ticks++;
+            this.Text = _ticks.ToString();
+        }
+
+        private void SetLevel()
+        {
+            this.comboBox1.Items.AddRange(this.game.LevelNames().ToArray());
+                
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.game.SetLevel(comboBox1.SelectedItem.ToString());
+            this.SetDisplayLevelName();
+        }
+    }
+}
